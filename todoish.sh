9c98481fb669f9133d49541cd81bd27a09b2d74d
AriPal <dler.hasan@drift.oslo.kommune.no>
2023-04-18 20:24:19 +0200

    finitooo

--- todoish.sh
+++ todoish.sh
@@ -1,32 +1,53 @@
 #!/bin/bash
 
-# Add todo task
-# Remove todo task
-# Mark todo as completed
-# List all todo tasks
-# List completed todo tasks
+# Add todo task [done]
+# Remove todo task []
+# Mark todo as completed []
+# List all todo tasks []
+# List completed todo tasks []
 
-#Define global variables
+# Global variables
+TODO_FILE="$(pwd)/todo.txt"
 
-# Define the file where we will store the todo tasks
-# TODO_FILE="$(pwd)/todo.txt"
-
+# Create the file if it doesn't exist
 if [ ! -e "$TODO_FILE" ]; then
     # Welcome message
-    fortune | cowsay -f tux "Welcome $(whoami) to todoish, let's get stuff done!"
+    figlet "Todoish!"
+    fortune | cowsay -f tux "A simple task manager living inside your command line"
     touch $TODO_FILE
 fi
 
-
+# Action to perform from user input i.e. source add "name of the task"
 case "$1" in
-    "add")
-        echo "Task added"
+"add")
+    # add task to text file
+    echo "$2" >>$TODO_FILE
+    echo "Task added"
     ;;
-    2|3)
-        echo "item = 2 or item = 3"
+"tasks")
+    cat $TODO_FILE
     ;;
-    *)
-        echo "default (none of above), result $action"
+"remove")
+    # remove task from text file
+    echo "remove task"
+    ;;
+"show-completed")
+    # show completed tasks
+    echo "show completed tasks"
     ;;
 esac
-    
+
+help() {
+    less help.txt
+}
+
+# Get option argument from input i.e. source -h
+while getopts "h" option; do
+    case $option in
+    h) help ;;
+    ?)
+        echo "Usage: %s: [-a] [-b value] args\n" $0
+        exit 2
+        ;;
+    esac
+done
